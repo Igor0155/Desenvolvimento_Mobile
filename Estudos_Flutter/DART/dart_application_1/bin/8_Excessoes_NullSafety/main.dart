ca37cc0a75107b30e44443a7b7d7bfb0d32aaf65
igor0155 <igorgabriel0155@gmail.com>
2023-04-11 10:35:11 -0300

    teste

--- Estudos_Flutter/DART/dart_application_1/bin/8_Excessoes_NullSafety/main.dart
+++ Estudos_Flutter/DART/dart_application_1/bin/8_Excessoes_NullSafety/main.dart
@@ -1,10 +1,49 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'controllers/bank_controller.dart';
 import 'exceptions/bank_controller_exceptions.dart';
 import 'models/account.dart';
 
+void testingNullSafety() {
+  Account? myAccount;
+
+  //Simulando uma conexão externa
+  Random rng = Random();
+  int rngNum = rng.nextInt(10);
+
+  if (rngNum <= 5) {
+    myAccount = Account(name: 'Tesla', balance: 200, isAuthenticated: true);
+  }
+
+  print(myAccount.runtimeType);
+
+  // Não funciona aqui
+  // print(myAccount.balance);
+
+  //'!' Força pegar um valor, mesmo que ele pode receber nulo
+  //print(myAccount!.balance);
+
+  // // If comum
+  // if (myAccount != null) {
+  //   print(myAccount.balance);
+  // } else {
+  //   print('Conta Nula');
+  // }
+
+  //if else compactado:
+  //    Se Cont != null '?' = true   ':' = false
+  print(myAccount != null ? myAccount.balance : "Conta Nula");
+
+  //Chamada segura
+  //caso for null ele retorna = 'null'
+  print(myAccount?.balance);
+}
+
 void main() {
+  testingNullSafety();
+
+/*
   assert(false, "testeFalso");
   // Criando o banco
   BankController bankController = BankController();
@@ -46,6 +85,8 @@ void main() {
   } on Exception {
     print('Algo deu errado na transação');
   }
+
+  */
 }
 
 // void main() {
