01d35f8dd2dc57119bd0c51d8e2dad0920a5a978
igor0155 <igorgabriel0155@gmail.com>
2024-03-12 15:14:19 -0300

    Atualizando

--- Estudos_Flutter/dart_2024/bin/main.dart
+++ Estudos_Flutter/dart_2024/bin/main.dart
@@ -2,6 +2,8 @@
 // import 'package:meta/meta.dart' show required;
 
 // "as" diz que vamos atribuir essa biblioteca no nome "meta"
+import 'dart:convert';
+
 import 'package:meta/meta.dart' as meta;
 
 // Declarações de funções e variaveis
@@ -26,12 +28,29 @@ String msm({@meta.required String? sugeito, @meta.required String? msg}) =>
 
 // Classes
 // @meta.Immutable() Impede que os parametros sejá imutaveus (Da erro)
-@meta.Immutable()
-class UserModel {
+// @meta.Immutable()
+class _UserModel {
+  // CONST NÃO PODE SER USADO DENTRO DA CLASS
+
   final String name;
   final String age;
 
-  UserModel(this.name, this.age);
+  _UserModel(this.name, this.age);
+
+  //supondo que recebemos um json de uma api
+  //                      CHAVE x VALOR nome
+  _UserModel.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        age = json['age'];
+
+// Recebi uma string com um json dentro
+
+  factory _UserModel.fromJsonString(String json) {
+    // decodificar string json no formato map
+    // pega a string e tranforma em json
+    final map = jsonDecode(json);
+    return _UserModel.fromJson(map);
+  }
 }
 
 main() {
@@ -43,5 +62,8 @@ main() {
 
   final String msg = msm(sugeito: 'Teste', msg: 'teste2');
 
-  print(msg);
+  print('$msg');
 }
+
+
+//Gerenciadores de estados: BloC, MobX GetX
